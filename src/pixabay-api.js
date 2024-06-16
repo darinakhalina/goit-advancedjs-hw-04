a9f74140d55ed57ecb944a07ab8235ad9db8fce9
Darina Khalina <darina.khalina@namecheap.com>
2024-06-16 17:17:00 +0300

    fix service images

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -3,17 +3,17 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '44415141-73aa0910a2180b32b461f4de8';
 
-export async function searchImages(searchInputValue, page, pageSize) {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: searchInputValue,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: pageSize,
-      page,
-    },
+export async function serviceImages(searchQuery, pageSize, page) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: pageSize,
+    page,
   });
+
+  const response = await axios.get(`${BASE_URL}?${params.toString()}`);
   return response.data;
 }
